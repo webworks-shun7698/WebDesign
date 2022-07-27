4bee3562f48cae01f5e3480617b6b76d83308c0b
webworks-shun7698 <webWorks.shun7698@gmail.com>
2022-07-27 22:43:05 +0900

    20220727

--- js/add-class.js
+++ js/add-class.js
@@ -1,14 +1,12 @@
 //スクロールの度にターゲット要素のページ上の位置を算出する
 document.addEventListener('scroll', function(e){
-	var elem = document.getElementsByClassName('thumbnail');
+	var elem = document.getElementsByClassName('add-target');
 	var clientRect = elem[0].getBoundingClientRect();
 	var windowHeight = window.innerHeight;
 
-	if (clientRect.top < windowHeight) {
-		console.log('つけるよ');
-	} else if (clientRect.bottom < 0) {
-		console.log('消すよ');
+	if ((clientRect.top < windowHeight) && (clientRect.bottom > 0)) {
+		elem[0].classList.add('add-class');
 	} else {
-
+		elem[0].classList.remove('add-class');
 	}
 });
